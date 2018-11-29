479d87e5d4810536419eb8672d0cf554cbd13bb6
Jesus Ramirez <drjesusf@gmail.com>
2018-11-28 20:13:55 -0500

    create springboot project

---
+++ src/main/java/banking/ads/ServletInitializer.java
@@ -0,0 +1,12 @@
+package banking.ads;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(bankingMonolithicSboot.class);
+	}
+}
---
+++ src/main/java/banking/ads/bankingMonolithicSboot.java
@@ -0,0 +1,12 @@
+package banking.ads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class bankingMonolithicSboot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(bankingMonolithicSboot.class, args);
+	}
+}
