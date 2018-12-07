d9d27a2cae1c8a5e444fb9ec6bb41b3c210c51e6
Jesus Ramirez <drjesusf@gmail.com>
2018-12-07 17:16:01 -0500

    add jwt

--- src/main/java/banking/ads/bankingMonolithicSboot.java
+++ src/main/java/banking/ads/bankingMonolithicSboot.java
@@ -2,24 +2,12 @@ package banking.ads;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
-//@ServletComponentScan
 @SpringBootApplication
 public class bankingMonolithicSboot {
 
 	public static void main(String[] args) {
 		SpringApplication.run(bankingMonolithicSboot.class, args);
-	}
-	
-	@Bean
-	public FilterRegistrationBean getFilter() {
-		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-	    registrationBean.setFilter(new JwtFilter());
-	    registrationBean.addUrlPatterns("/secure/*");
-
-	    return registrationBean;
 	}
 	
 }
