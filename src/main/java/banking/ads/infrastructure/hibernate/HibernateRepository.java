5c40d85b247f8cfa05035a081a05f71262e54486
Vladimir Espinoza <vaes0606@gmail.com>
2018-12-11 21:17:33 -0500

    Completando HibernateRepository

--- src/main/java/banking/ads/infrastructure/hibernate/HibernateRepository.java
+++ src/main/java/banking/ads/infrastructure/hibernate/HibernateRepository.java
@@ -1,31 +1,52 @@
 package banking.ads.infrastructure.hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import seedWork.IRepository;
 
 public class HibernateRepository<T> implements IRepository<T> {
+	protected SessionFactory sessionFactory;
+	
+	@Autowired
+	public void setSessionFactory(final SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	protected Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
 
-	@Override
-	public T get(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public T persist(T entity) {
+		getSession().persist(entity);
+		return entity;
 	}
 
 	@Override
-	public T get(long id) {
-		// TODO Auto-generated method stub
-		return null;
+	public T save(T entity) {
+		getSession().save(entity);
+		return entity;
 	}
 
 	@Override
-	public void saveOrUpdate(T entity) {
-		// TODO Auto-generated method stub
-		
+	public T update(T entity) {
+		getSession().update(entity);
+		return entity;
 	}
 
 	@Override
-	public void delete(T entity) {
-		// TODO Auto-generated method stub
-		
+	public T merge(T entity) {
+		getSession().merge(entity);
+		return entity;
 	}
+
+	@Override
+	public T saveOrUpdate(T entity) {
+		getSession().saveOrUpdate(entity);
+		return entity;
+	}
+
+	
 
 }
